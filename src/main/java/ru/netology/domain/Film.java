75ddd94d723969b25f606c46bbd380bbac6ba475
Nikita Novoselov <sarkomath@outlook.com>
2021-01-28 21:47:24 +1000

    format code. verifying

--- src/main/java/ru/netology/domain/Film.java
+++ src/main/java/ru/netology/domain/Film.java
@@ -7,9 +7,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 
 public class Film {
-  // убираем лишние поля, т.к. дата-полей в задании нужно не много
-  private int id; // id в менеджере
-  private String Date ; // дата выхода
-  private String Genre; // жанр
-  private String productName; // название фильма
+    // убираем лишние поля, т.к. дата-полей в задании нужно не много
+    private int id; // id в менеджере
+    private String Date; // дата выхода
+    private String Genre; // жанр
+    private String productName; // название фильма
 }
